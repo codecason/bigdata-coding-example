e666b28f189ba53fbdccf8cd0a3e5509ea9d16c3
codecason <somebody@gmail.com>
2021-07-29 22:47:28 +0800

    update initial for hive

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # coding417-examples
 
 企业级大数据平台架构建设实战各章节示例代码# bigdata-coding-example
+
+# 用docker搭建hadoop开发环境
+[从 0 开始使用 Docker 快速搭建 Hadoop 集群环境](https://juejin.cn/post/6844903471607611406)
+
--- start-hadoop.sh
+++ start-hadoop.sh
@@ -5,7 +5,7 @@
 
 export HADOOP_HOME=/usr/local/hadoop
 
-cd ${HADOOP_HOME}/bin && ./hdfs namenode -format
+cd ${HADOOP_HOME}/bin && ./hdfs namenode -format # 格式化HDFS
 cd ${HADOOP_HOME}/sbin && ./start-dfs.sh && ./start-yarn.sh # 启动YARN
 jps
 
@@ -16,3 +16,23 @@ hdfs dfs -mkdir /hive/log && \
 hdfs dfs -mkdir /hive/tmp && \
 hdfs dfs -chmod -R 777 /hive
 
+# hive start
+前置条件:
+${HIVE_HOME}/conf/hive-site.xml
+
+hive --service metastore &
+hive --service hiveserver2 &
+
+# 最终的 bashrc 配置结果
+export JAVA_HOME=/usr/local/jdk
+export HADOOP_HOME=/usr/local/hadoop
+export HIVE_HOME=/home/hadoop/soft/apache-hive-2.3.9-bin
+export PATH=${JAVA_HOME}/bin:$PATH
+export PATH=${JAVA_HOME}/jre/bin:$PATH
+export PATH=${HIVE_HOME}/bin:$PATH
+export PATH=${HADOOP_HOME}/bin:$PATH
+export PATH=${HADOOP_HOME}/sbin:$PATH
+
+# hive连接
+hive
+> show tables;
